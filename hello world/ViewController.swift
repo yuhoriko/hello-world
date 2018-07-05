2980c3d35e77d4ad44bf578ee541a188bfb52e79
GitHub yuhoriko <pon.de.yuri@gmail.com>
2018-07-05 14:34:10 +0900

    コミットするよ

--- hello world/ViewController.swift
+++ hello world/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var label: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +21,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func tapButton(_ sender: Any) {
+        label.text="hello world"
+    }
+    
 }
 
